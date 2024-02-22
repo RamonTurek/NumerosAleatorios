d0efe28ac74b21cdef1fee5ded196ff119645a19
Ramon Turek de Mello <125655966+RamonTurek@users.noreply.github.com>
2024-02-22 08:56:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let resultado = Math.floor(Math.random() * 100);
+let valor;
+do{
+    valor = prompt("Digite um número de 0 a 100")
+
+    if (valor < resultado){
+        alert("Muito baixo");
+    }else if(valor > resultado){
+        alert("Muito alto");
+    }
+
+}while(valor != resultado)
+
+if (valor == resultado){alert("Valor correto!!");}
+
+
